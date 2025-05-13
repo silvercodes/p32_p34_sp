f89fc790fdd981c0243521f299ff9ae4d87221d7
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-05-13 13:09:18 +0300

    initial

---
+++ 01_process/01_process/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// 1. Процесс (Process)
+
+// 2. Поток (Thread)
+
+// 3. Адрессное пространство (Memory Scope)
+
+// 4. Приложение (Application)
+
+// 5. Сборка (Assembly)
+
+// 6. Модуль (Module)
+
+// 7. Системные ресурсы (System resources)
+
+
+
+#region Process
+
+
+
+
+
+
+#endregion
+
+
+
